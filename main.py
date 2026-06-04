7ab2436cc8bc149ae559c962b12de9be37d38982
Lev1106 <lev2.blsv@gmail.com>
2026-06-04 12:07:14 +0500

    Fixed polling entrypoint

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-from app import *
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from telegram import Update
+
+from app import telegram_app
+
+
+def main():
+    telegram_app.run_polling(allowed_updates=Update.ALL_TYPES)
+
 
 if __name__ == "__main__":
-    #start()
-    telegram_app.run_polling()
+    main()
